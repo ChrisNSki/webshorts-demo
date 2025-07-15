6c1c595096c50d83a0f408079ad39417fba49602
Christopher Narowski <chris@ensif.com>
2025-07-15 02:29:22 -0600

    typo (capitalization) issues, re-deploying

--- src/app/docs/docs-pages/index.jsx
+++ src/app/docs/docs-pages/index.jsx
@@ -5,8 +5,8 @@ import License from './License';
 import ConfiguringWebShorts from './ConfiguringWebShorts';
 import WebShortsProvider from './WebShortsProvider';
 import ShortcutListeners from './ShortcutListeners';
-import WebShortsDialog from './WebShortsDialog';
+import WebshortsDialog from './WebshortsDialog';
 import CustomStyling from './CustomStyling';
 import UseShortcutsHooks from './UseShortcutsHooks';
 
-export { QuickStart, Features, Compatibility, License, ConfiguringWebShorts, WebShortsProvider, ShortcutListeners, WebShortsDialog, CustomStyling, UseShortcutsHooks };
+export { QuickStart, Features, Compatibility, License, ConfiguringWebShorts, WebShortsProvider, ShortcutListeners, WebshortsDialog, CustomStyling, UseShortcutsHooks };
